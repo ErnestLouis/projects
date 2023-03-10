383afd22d182573fe207f3266c14b0db4a63b82d
Ernest <ernest_louis@outlook.com>
2023-03-10 11:38:08 -0600

    add definition up to insert sort

--- dsa/linked_list.cpp
+++ dsa/linked_list.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+using std::cin;
+using std::cout;
+using std::endl;
+
+int arr_size(int A[]);
+
 template <class Type>
 class Node
 {
@@ -25,7 +31,8 @@ public:
 	void display();
 	void rec_display();
 	void insert(int index, T data);
-	void insert_sort();
+	void insert_sort(T data);
+	void append(T data);
 	void reverse();
 	void rec_reverse();
 
@@ -34,12 +41,13 @@ public:
 	T sum();
 	T max();
 	T min();
-	T linear_search();
+	T linear_search(T key);
 
 	bool is_sort();
 	bool has_loop();
 	double avg();
 	int length();
+	int rec_length();
 
 	LinkedList* concat(LinkedList<T>& arr2);
 	LinkedList* merge(LinkedList<T>& arr2);
@@ -50,11 +58,51 @@ public:
 
 
 
+
 int main()
 {
-	int A[] = { 2,4,6,8,10 };
+	int A[] = { 2,3,4,5};
 
-	LinkedList Link01(A, 5);
+	int num = 6;
+
+	LinkedList link01(A, arr_size(A));
+
+	link01.display();
+
+	link01.insert_sort(num);
+
+    link01.display();
+
+
+	/*
+	cout << " before append: \n";
+
+	link01.display();
+
+	std::cout << endl;
+
+	link01.append(num);
+
+	std::cout << "Appended " << num << " into last index \n";
+
+	link01.display();
+
+	*/
+
+
+/*
+	cout << " before insertion: \n";
+
+	link01.display();
+
+	std::cout << endl;
+
+	link01.insert(5,num);
+
+	std::cout << "Insert " << num << " into index 5: \n";
+
+	link01.display();
+*/
 
 
 
@@ -85,7 +133,7 @@ LinkedList<T>::LinkedList(T Arr[], int num_of_elements)
 	for (int i = 1; i < num_of_elements; i++)
 	{
 		new_node = new Node<T>;
-		new_node->data = Arr[0];
+		new_node->data = Arr[i];
 		new_node->next = NULL;
 		last->next = new_node;
 		last = new_node;
@@ -109,15 +157,27 @@ LinkedList<T>::~LinkedList()
 template<class T>
 void LinkedList<T>::display()
 {
+	Node<T>* p = first;
 
-
-
+	while (p)
+	{
+		cout << p->data << " ";
+		p = p->next;
+	}
+	cout << endl;
 }
 
 template<class T>
 void LinkedList<T>::rec_display()
 {
+	static Node<T>* p = first;
 
+	if (p != NULL)
+	{
+		cout << p->data << " ";
+		p = p->next;
+		rec_display();
+	}
 
 
 }
@@ -125,17 +185,83 @@ void LinkedList<T>::rec_display()
 template<class T>
 void LinkedList<T>::insert(int index, T data)
 {
+	Node<T>* new_node, *p = first;
 
+	if (index < 0 || index > length())
+	{
+		return;
+	}
+	new_node = new Node<T>;
+	new_node->data = data;
+	new_node->next = NULL;
 
+	if (index == 0)
+	{
+		new_node->next = first;
+		first = new_node;
+	}
+	else
+	{
+		for (int i = 0; i < index - 1; i++)
+		{
+			p = p->next;
+		}
+		new_node->next = p->next;
+		p->next = new_node;
+	}
 
 }
 
 template<class T>
-void LinkedList<T>::insert_sort()
+void LinkedList<T>::append( T data)
 {
+	Node<T>* new_node, * p = first;
+	int index = length() - 1;
 
+	new_node = new Node<T>;
+	new_node->data = data;
+	new_node->next = NULL;
 
+	for (int i = 0; i < index; i++)
+	{
+		p = p->next;
+	}
+	new_node->next = p->next;
+	p->next = new_node;
 
+}
+
+template<class T>
+void LinkedList<T>::insert_sort(T data)
+{
+	Node<T>* new_node, * p = first, * q = NULL;
+
+	new_node = new Node<T>;
+	new_node->data = data;
+	new_node->next = NULL;
+
+	if (first == NULL)
+	{
+		first = new_node;
+	}
+	else
+	{
+		while (p && p->data < data)
+		{
+			q = p;
+			p = p->next;
+		}
+		if (p == first)
+		{
+			new_node->next = first;
+			first = new_node;
+		}
+		else
+		{
+			new_node->next = q->next;
+			q->next = new_node;
+		}
+	}
 }
 
 template<class T>
@@ -195,11 +321,22 @@ T LinkedList<T>::min()
 }
 
 template<class T>
-T LinkedList<T>::linear_search()
+T LinkedList<T>::linear_search(T key)
 {
+	Node<T>* p = first;
 
+	int i = 0;
+	while (p)
+	{
+		if (key == p->data)
+		{
+			return i;
+		}
+		i++;
+		p = p->next;
+	}
 
-
+	return NULL;
 }
 
 template<class T>
@@ -229,9 +366,33 @@ double LinkedList<T>::avg()
 template<class T>
 int LinkedList<T>::length()
 {
+	Node<T>* p = first;
+	int count = 0;
 
+	while (p)
+	{
+		count++;
+		p = p->next;
+	}
 
+	return count;
+}
 
+template<class T>
+int LinkedList<T>::rec_length()
+{
+	static Node<T>* p = first;
+	//int count = 0;
+
+	if (p == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		p = p->next;
+		return rec_length() + 1;
+	}
 }
 
 template<class T>
@@ -250,3 +411,25 @@ LinkedList<T>* LinkedList<T>::merge(LinkedList<T>& arr2)
 
 }
 
+int arr_size(int A[])
+{
+	int j = 0;
+	int* a = new int[100];
+
+	for (int i = 0; i < 100; i++)
+	{
+		a[i] = NULL;
+	}
+
+	a[0] = A[0];
+
+	while (A[j] > -100)
+	{
+		a[j] = A[j];
+		j++;
+	}
+	delete[] a;
+
+
+	return j;
+}
